82bc0053fde05cab03e54c557332321bc3275bc5
Zhuzhgin <artemfox@list.ru>
2021-12-17 15:37:14 +0500

    Initial Commit

---
+++ loginUserNameInterface/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginUserNameInterface
+//
+//  Created by Artem Zhuzhgin on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
