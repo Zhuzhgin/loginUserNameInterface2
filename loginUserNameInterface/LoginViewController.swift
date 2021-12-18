4dffbee222c6be9206b4b7ee0275d489030c50bb
Zhuzhgin <artemfox@list.ru>
2021-12-18 10:45:45 +0500

    1

---
+++ loginUserNameInterface/LoginViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  loginUserNameInterface
+//
+//  Created by Artem Zhuzhgin on 17.12.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    // MARK: - Private Properties
+    private let userName = "Art"
+    private let password = "000"
+    
+    // MARK: - Override methods
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard let welcomeVC = segue.destination as? WelcomeViewController else {return}
+            welcomeVC.userName = userNameTF.text!
+        }
+        
+    // MARK: IB Action
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        guard userNameTF.text == userName, passwordTF.text == password  else {
+            passwordTF.text = ""
+            viewAlert(title: "Error", message: "Incorrect password or name of user", tag: 0)
+            return
+        }
+        performSegue(withIdentifier: "goToWelcomeVC", sender: nil)
+    }
+    
+    @IBAction func helpButtonPressed(_ sender: UIButton) {
+        switch sender.tag {
+        case 1: viewAlert(title: "fogot user name?", message: "Input user name?", tag: 1)
+        default: viewAlert(title: "fogot password", message: "Input pfssword?", tag: 2)
+        }
+    }
+    
+    @IBAction func unwindToLoginVC(_ unwindSegue: UIStoryboardSegue) {
+            userNameTF.text = ""
+            passwordTF.text = ""
+    }
+}
+
+extension LoginViewController {
+    
+    func viewAlert (title: String, message: String, tag: Int) {
+        let alert =  UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            switch tag {
+            case 0: self.passwordTF.text = ""
+            case 1 : self.userNameTF.text = self.userName
+            default : self.passwordTF.text = self.password
+            }
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        self.view.endEditing(true)
+    }
+}
+
---
+++ loginUserNameInterface/WelcomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WelcomeViewController.swift
+//  loginUserNameInterface
+//
+//  Created by Artem Zhuzhgin on 17.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome \(userName)"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
